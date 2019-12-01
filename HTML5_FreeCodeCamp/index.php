466e1cec22dba3995045eb435831eb75e1e331f3
Quang Bui <quangbui@Quangs-MacBook-Pro.local>
2019-11-30 22:27:24 -0500

    First Commit

---
+++ HTML5_FreeCodeCamp/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+
+	<form action="" method="">
+		<label for="name">First name</label>
+		<input type="text" name="name" id="name">
+		<br>
+		<p>password</p>
+		
+		<br>
+		<input type="password" placeholder="password">
+		<br>
+		<input type="email" name="" id="" value="email@email">
+		
+		<br>
+		<p>Description</p>
+		<textarea name="" id="" cols="20" rows="10"></textarea>
+		
+		<br>
+		<p>Please select your favarite coding language?</p>
+		<input type="radio" name="coding" id="" value="javascript">Javascript
+		<input type="radio" name="coding" id="" value="java">Java
+		<input type="radio" name="coding" id="" value="python">Python
+		
+		<br>
+		<p>Please select your favarite coding language?</p>
+		<input type="checkbox" name="programming" id="" value="javascript" checked="checked">Javascript
+		<input type="checkbox" name="programming" id="" value="java">Java
+		<input type="checkbox" name="programming" id="" value="python">Python
+		
+		<br>
+		<p>Please select your favarite coding language?</p>
+		<select name="language" id="">
+			<option value="javascript">Javascript</option>
+			<option value="java">Java</option>
+			<option value="python">Python</option>
+		</select>
+		<br>
+		<input type="submit" name="" value="Save">
+		<!--<button type="submit">Sumit</button>-->
+	</form>
+
+	<!--
+		<ul>
+			<li>John</li>
+			<li>Peter</li>
+				<ol>
+					<li>html</li>
+					<li>css</li>
+				</ol>
+			<li>Sarah</li>
+			<li><a href="">home</a></li>
+			<li><a href="">about</a></li>
+			<li><a href="">contact</a></li>
+		</ul>
+
+		<h1>copyright &copy;</h1>
+
+	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <strong>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</strong> but also <em>the leap into electronic typesetting</em>, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+
+	<h1>Hello I'm 1<sup>st</sup> john <sub>in town</sub></h1>
+
+
+	<a href="#"><img src="pictures/laptop" width="259" height="173" alt="empty link"></a>
+	<h1 id="top">Top of the page</h1>
+	<a href="about.php">about</a>
+	<h3>Image of Udemy logo</h3>
+	<img src="pictures/udemy" alt="the text for img if the img cannot display">
+	<a target="_blank" href="https://www.udemy.com">Visit "Udemy" for more information.</a>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<p>some text here</p>
+	<a href="#top">back to the top</a>
+
+
+	<br>
+	<br>
+	<h3>Image of Pexels laptop</h3>
+	<img src="pictures/laptop" width="259" height="173" alt="Laptop of Pexels">
+
+
+	<h1>I'm in heading 1</h1>
+	<h2>I'm in heading 2</h2>
+	<h3>I'm in heading 3</h3>
+	<h4>I'm in heading 4</h4>
+	<h5>I'm in heading 5</h5>
+	<h6>I'm in heading 6</h6>
+	<h3>hello         world</h3>
+	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+-->
+</body>
+</html>
